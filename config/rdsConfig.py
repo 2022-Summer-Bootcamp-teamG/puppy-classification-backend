d6c2826507a5285a17f44c2aa70eca612e84f238
qkrgmlrud00 <qkrgmlrud00@gmail.com>
2022-07-18 15:05:44 +0900

    feat: setting RDS

--- config/rdsConfig.py
+++ config/rdsConfig.py
@@ -1,15 +1,25 @@
+import logging
+import sys
+
 import pymysql
 
 RDS_HOST = 'end_point'
-RDS_DATABASE = 'summer-bootcamp-2022'
+RDS_DATABASE = 'sys'
 RDS_USER = 'admin'
 RDS_PASSWORD = 'password'
 
-def getConnection():
-    return pymysql.connect(
-        host=RDS_HOST,
-        user=RDS_USER,
-        db=RDS_DATABASE,
-        password=RDS_PASSWORD,
-        charset='utf8'
-    )
+
+def RDSConnected():
+    try:
+        conn = pymysql.connect(
+            host=RDS_HOST,
+            user=RDS_USER,
+            db=RDS_DATABASE,
+            passwd=RDS_PASSWORD,
+            port=3306,
+            charset='utf8')
+        cur = conn.cursor()
+    except:
+        logging.error("RDS 연결 안됨")
+        sys.exit(1)
+    return conn, cur
